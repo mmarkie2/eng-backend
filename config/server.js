4df629a2ea39002113a247f17b271ad59f04bbc7
mm <maciej.markiewicz10@gmail.com>
2021-10-20 21:23:15 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '50681e67c158d7e78d644e5ea7eb417e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17ac0399-96a0-4bfb-abbc-b61ed2864256'
+};
